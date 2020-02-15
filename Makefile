deefb952decd9b327952ff7db906de1f96428a68
I-VINCI\eta.aoki <eta.aoki@i-vinci.co.jp>
2020-02-15 16:41:13 +0900

    [add] serverlessのDeploy構成を追加。

--- Makefile
+++ Makefile
@@ -1,9 +1,15 @@
-.PHONY: build
+.PHONY: build deploy destroy
 
 lambda-select-path=./cmd/lambda-select
 lambda-insert-path=./cmd/lambda-insert
 
 
 build:
-	go build -o $(lambda-select-path)/lambda-select-bin $(lambda-select-path)
-	go build -o $(lambda-insert-path)/lambda-insert-bin $(lambda-insert-path)
+	go build -ldflags="-s -w" -o ./bin/lambda-select $(lambda-select-path)
+	go build -ldflags="-s -w" -o ./bin/lambda-insert $(lambda-insert-path)
+
+deploy: build
+	serverless deploy --verbose
+
+destroy:
+	serverless remove --verbose
--- cmd/lambda-select/main.go
+++ cmd/lambda-select/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// APIResponse is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+//
+// https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
+type APIResponse events.APIGatewayProxyResponse
+
 // SelectHandler : Lambdaを介して、RDSのデータをセレクトして、フロント返却します。
-func SelectHandler(ctx context.Context) (*Response, error) {
-	res := &Response{
+func SelectHandler(ctx context.Context) (APIResponse, error) {
+	var buf bytes.Buffer
+
+	entity := &Response{
 		ID:      "R00001",
 		Author:  "atEaE",
 		Message: "Hello World",
+	}
+	jbuf, err := json.Marshal(entity)
+
+	if err != nil {
+		return APIResponse{StatusCode: 404}, err
+	}
+	json.HTMLEscape(&buf, jbuf)
+
+	res := APIResponse{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            buf.String(),
+		Headers: map[string]string{
+			"Content-Type":         "application/json",
+			"X-SysExam-Func-Reply": "select-handler",
+		},
 	}
 	return res, nil
 }
